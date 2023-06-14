0aa5733b1531aa86d49d7902de698ae2634c6421
Дмитрий Соловьёв <real.dj.ray@gmail.com>
2023-06-14 19:17:05 +0300

    Lesson 2

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-menu = ["Installation", "First application", "Feedback"]
+menu = [{"name": "Installation", "url": "install-flask"},
+        {"name": "First application", "url": "first-app"},
+        {"name": "Feedback", "url": "contact"}]
 
 
 @app.route('/')
@@ -16,5 +17,19 @@ def about():
     return render_template('about.html', title="About the site", menu=menu)
 
 
+@app.route("/profile/<string:user_name>/<string:path>")
+def profile(user_name, path):
+    return f"User: {user_name}, path: {path}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        x = request.form['username']
+    return render_template('contact.html', title="Feedback", menu=menu)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,
+            use_reloader=False,
+            )
