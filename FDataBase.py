2c3c8a62bed0d5fe70e8049c8bc9b4f7018c5146
Дмитрий Соловьёв <real.dj.ray@gmail.com>
2023-06-22 18:00:29 +0300

    Lesson 3/2

---
+++ FDataBase.py
@@ -0,0 +1,131 @@
+import math
+import re
+import sqlite3
+import time
+
+from flask import url_for
+
+
+class FDataBase:
+    db = sqlite3.connect('Flask.db')
+    def init(self, db):
+        self.__db = db
+        self.__cur = self.__db.cursor()
+
+    def getMenu(self):
+        sql = '''SELECT * FROM mainmenu'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except:
+            print("Error reading from DB")
+        return []
+
+    def addPost(self, title, text, url):
+        try:
+            self.__cur.execute(
+                f"SELECT COUNT() as count FROM posts WHERE url LIKE '{url}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("An article with this url already exists")
+                return False
+
+            base = url_for('static', filename='images_html')
+
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          "\\g<tag>" + base + "/\\g<url>>",
+                          text)
+
+            tm = math.floor(time.time())
+            self.__cur.execute(
+                'INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)', (title, text, url, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error adding article to DB" + str(e))
+            return False
+
+        return True
+
+    def getPost(self, alias):
+        try:
+            self.__cur.execute(
+                f"SELECT title, text FROM posts WHERE url LIKE '{alias}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Error getting article from DB " + str(e))
+
+        return (False, False)
+
+    def getPostsAnonce(self):
+        try:
+            self.__cur.execute(
+                f"SELECT id, title, text, url FROM posts ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Error getting article from DB " + str(e))
+
+        return []
+
+    def addUser(self, name, email, hpsw):
+        try:
+            self.__cur.execute(f"SELECT COUNT() as count FROM users WHERE email LIKE '{email}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("User with this email already exists")
+                return False
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, NULL, ?)", (name, email, hpsw, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error adding user to DB " + str(e))
+            return False
+
+        return True
+
+    def getUser(self, user_id):
+        try:
+            self.__cur.execute(f'SELECT * FROM users WHERE id = {user_id} LIMIT 1')
+            res = self.__cur.fetchone()
+            if not res:
+                print('User not found')
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Error adding user to DB " + str(e))
+
+        return False
+
+    def getUserByEmail(self, email):
+        try:
+            self.__cur.execute(f'SELECT * FROM users WHERE email = "{email}" LIMIT 1')
+            res = self.__cur.fetchone()
+            if not res:
+                print("User not found")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Error adding user to DB " + str(e))
+
+        return False
+
+    def updateUserAvatar(self, avatar, user_id):
+        if not avatar:
+            return False
+
+        try:
+            binary = sqlite3.Binary(avatar)
+            self.__cur.execute(f'UPDATE users SET avatar = ? WHERE id = ?', (binary, user_id))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('DB avatar update error: ' + str(e))
+            return False
+        return True
--- app.py
+++ app.py
@@ -1,58 +1,84 @@
-from flask import Flask, render_template, request, flash, url_for, session, abort, redirect
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, url_for, flash, abort
+from FDataBase import FDataBase
+
+# configuration
+DATABASE ='/tmp/Flask.db'
+DEBUG = True
 
 app = Flask(__name__)
-
-menu = [{"name": "Installation", "url": "install-flask"},
-        {"name": "First application", "url": "first-app"},
-        {"name": "Feedback", "url": "contact"}]
-
+app.config.from_object(__name__)
 app.secret_key = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, '../Flask.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """Helper function for creating database tables"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """Connecting to the database if it is not already established"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title="About the site", menu=menu)
-
-
-@app.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(404)
-    return f'User profile: {username}'
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-    return render_template('login.html', title="Authorization", menu=menu)
-
-
-@app.errorhandler(404)
-def pageNotFound(error):
-    return render_template('page404.html', title='Page not found', menu=menu), 404
-
-
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == 'POST':
-        if len(request.form['username']) > 2:
-            flash("The message has been sent", category='success')
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Error adding article', category = 'error')
+            else:
+                flash('Article added successfully', category ='success')
         else:
-            flash("Send error", category='error')
+            flash('Error adding article', category = 'error')
+    return render_template('add_post.html', menu = dbase.getMenu(), title='Adding an article')
 
-    return render_template('contact.html', title="Feedback", menu=menu)
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the connection to the database if it was established"""
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 if __name__ == "__main__":
-    app.run(debug=True,
-            use_reloader=False,
-            )
+    app.run(
+        debug=True,
+        use_reloader=False,
+        )
